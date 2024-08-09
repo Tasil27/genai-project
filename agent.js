94f4e3c22dc2a211af6645021aa363bc7430d9e3
Tasil27 <chrissy.eick27@googlemail.com>
2024-08-09 11:47:19 +0200

    Adding first prompts to the agent.

--- agent.js
+++ agent.js
@@ -7,15 +7,17 @@ const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
 export class Agent {
     constructor(personality) {
         this.personality = personality;
-        this.observation = [];
-        this.action = [];
+        this.observation = ["Es ist ein schöner Tag heute"];
+        this.action = ["Ich habe gerade eine leckere Zimtschnecke gefuttert"];
         this.locations = [];
         this.planOfDay = "";
     }
 
     async simulateObservation(currentTime) {
         const completion = await openai.chat.completions.create({
-            messages: [{ role: "system", content: "You are a helpful assistant." }],
+            messages: [{ role: "system", content: this.personality },
+                { role: "user", content: ` Stelle dir vor , du bist diese Person: ${this.personality},Es ist ${currentTime} jetzt. Deine letzten Beobachtungen sind ${this.observation}. Deine letzten Handlungen sind ${this.action}. Basierend darauf, was gerade passiert, was du gerade gemacht hast und  deine letzte Beobachtungen, beschreibe deine Beobachtungen zu diesem Zeitpunkt? Sei kurz und beschränke dich auf maximal 3 Sätze.` }
+            ],
             model: "gpt-3.5-turbo",
           });
         
--- simulate.js
+++ simulate.js
@@ -1,4 +1,8 @@
 import { Agent } from "./agent.js";
 
-const agent = new Agent("");
-agent.simulateObservation();
+const agent = new Agent("Hermann Fritz ist ein 36-jähriger IT-Architekt und Arzt, der in Berlin lebt und dort eine einzigartige Kombination von Fähigkeiten und Interessen pflegt. Er verbringt seine Tage damit, komplexe IT-Systeme für ein führendes Krankenhaus zu entwerfen, wo er seine medizinische Expertise mit technologischer Innovation vereint, um die Gesundheitsversorgung zu verbessern. In seiner Freizeit widmet er sich mit Leidenschaft der Entwicklung von künstlicher Intelligenz, die darauf abzielt, benutzerfreundliche Softwarelösungen für den Gesundheitsbereich zu schaffen. Diese Projekte sind nicht nur eine berufliche Ambition, sondern auch eine Herzensangelegenheit für Benjamin, da er der Überzeugung ist, dass Technologie eine Schlüsselrolle in der Prävention und Behandlung von Krankheiten spielen kann. Neben seiner Arbeit ist Benjamin ein begeisterter Tierliebhaber und teilt sein Zuhause mit einer fröhlichen Gruppe von Meerschweinchen, die ihm unendliche Freude bereiten und ihn in stressigen Zeiten entspannen lassen. Trotz seines vollen Terminkalenders findet er immer Zeit, um an Hackathons teilzunehmen und sich mit Gleichgesinnten über die neuesten Trends in der KI-Entwicklung auszutauschen.");
+
+const currentTime = new Date('2024-01-01T10:00:00')
+
+
+agent.simulateObservation(currentTime);
